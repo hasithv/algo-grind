9b69909f8933f19b310a0fdcc3e1af5d867efec0
AV912 <88800484+AV912@users.noreply.github.com>
2022-06-27 15:29:14 -0500

    date dashed

--- get_ff_factors.py
+++ get_ff_factors.py
@@ -8,9 +8,8 @@ def get_ff_factors():
     df["Date"] = df["Date"].apply(lambda x: add_dashes(x))
     df.to_csv("3_factors.csv", index=False, line_terminator="", encoding='utf-8')
 
-def add_dashes(x):
-    d = str(x)
-    return '%s-%s-%s' % (d[:4], d[4:6], d[6:])
+def add_dashes(d):
+    return '%s-%s-%s' % (d[8:], d[5:7], d[:4])
 
 
 if __name__ == '__main__':
