59bebb1065ade969e0ac849603379f15fd495200
Hasith <hasith200311@gmail.com>
2022-06-30 21:15:33 -0500

    switched to scikit learn

--- fama.py
+++ fama.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import linear_model
 import statsmodels.api as sm
 
 
@@ -9,28 +10,22 @@ def run_fama(ticker, months):
     ticker_df = ticker_df.drop(ticker_df.columns[0], axis=1)
     factors_df = pd.read_csv("month_factors.csv", index_col="Date")
 
-    first_date = max(ticker_df.index[0], factors_df.index[0])
+    first_date = max(ticker_df.index[1], factors_df.index[1])
     last_date = min(ticker_df.index[-1], factors_df.index[-1])
 
     ticker_df = ticker_df[first_date:last_date]
-    ticker_df = ticker_df[first_date:last_date]
+    factors_df = factors_df[first_date:last_date]
     ticker_df["ER"] = np.nan
 
     if ticker_df.shape[0] >= 24:
         for i in range(months, ticker_df.shape[0]):
-            X = factors_df[i - months:i][["Mkt-RF", "SMB", "HML"]]
-            y = ticker_df[i - months:i]["roi"] - factors_df[i - months:i]["RF"]
+            X = factors_df[i - months:i][["Mkt-RF", "SMB", "HML"]].values
+            y = ticker_df[i - months:i]["roi"] - factors_df[i - months:i]["RF"].values
 
-            X = sm.add_constant(X)
-            ff_model = sm.OLS(y, X, missing="drop").fit()
-            intercept, b1, b2, b3 = ff_model.params
+            model = linear_model.LinearRegression()
+            model.fit(X, y)
 
-            rf = factors_df[i - 1:i]['RF'].mean()
-            market_premium = factors_df[i - 1:i]['Mkt-RF'].mean()
-            size_premium = factors_df[i - 1:i]['SMB'].mean()
-            value_premium = factors_df[i - 1:i]['HML'].mean()
-
-            ticker_df["ER"][i] = rf + b1 * market_premium + b2 * size_premium + b3 * value_premium
+            ticker_df["ER"][i] = model.predict([factors_df.iloc[i-1][0:3]])
 
         rx = ticker_df["roi"]
         ry = ticker_df["ER"]
@@ -38,8 +33,6 @@ def run_fama(ticker, months):
         rmodel = sm.OLS(ry, rx, missing="drop").fit()
 
         return ticker_df, rmodel.rsquared
-
-    # intercept, b1, b2, b3 = ff_model.params
 
 
 if __name__ == "__main__":
@@ -69,7 +62,7 @@ if __name__ == "__main__":
 
             analysis_df.loc[ticker] = [ff_model.rsquared, ff_model.rsquared_adj]
 """
-    x = run_fama("AAPL", 12)[0]
+    x = run_fama("MS-PF", 12)[0]
 
     plt.subplot(211)
     plt.bar(x.index, x["ER"], label="ER", alpha=0.5)
--- main.py
+++ main.py
@@ -5,29 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    """
     full_df = pd.DataFrame(columns=["ticker", "max-month", "max-r^2"])
     full_df["ticker"] = pd.read_csv("constituents.csv")["ticker"]
     full_df = full_df.set_index("ticker")
 
     for ticker in full_df.index:
-        try:
-            months_df = pd.DataFrame(columns=["month range", "r^2"])
-            for m in range(2, 19):
-                months_df.loc[m-2] = [m, fama.run_fama(ticker, m)[1]]
+        months_df = pd.DataFrame(columns=["month range", "r^2"])
+        for m in range(2, 19):
+            months_df.loc[m-2] = [m, fama.run_fama(ticker, m)[1]]
 
-            full_df.loc[ticker] = months_df.loc[months_df['r^2'].idxmax()]
-        except:
-            print(ticker)
+        full_df.loc[ticker] = months_df.loc[months_df['r^2'].idxmax()]
 
     # x = fama.run_fama("MSFT", 12)[0]
 
     """
-    plt.subplot(211)
-    plt.bar(x.index, x["ER"], label="ER", alpha=0.5)
-    plt.bar(x.index, x["roi"], label="roi", alpha=0.5)
-    plt.legend(loc="upper right")
-
-    plt.subplot(212)
-    plt.scatter(x["roi"], x["ER"])
-    plt.show()
-    """
+    x, r2 = fama.run_fama("MSFT", 12)
